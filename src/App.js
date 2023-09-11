dc619738caf47159df2659beaf4e87572c90dcbb
2023-026 EomJiHee <jheom@ecounterp.co.kr>
2023-09-11 20:55:59 +0900

    2.4.5 undefined를 렌더링하지 않기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import { Fragment } from 'react';
 
 function App() {
-  const name = '뤼왝트';
-  return <div>{name === '리액트' ? <h1>리액트입니다.</h1> : <h2>리액트가 아닙니다.</h2>}</div>;
+  const name = undefined;
+  return <div>{name || '리액트'}</div>;
 }
 
 export default App;
